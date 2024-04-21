23bdc533a57b1ee2e61048ae4ea76d7581094792
tylerross6 <tylerross6@yahoo.com>
2024-04-21 17:49:52 +0000

    adding bash scripts to repo

---
+++ bashscripts/echo1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Hello World! This is my first Bash Script -- Lost"
+echo
+echo "This script will print and list our current directory."
+echo "It will also tell us which user we are logged into."
+echo
+echo "You are currently in:"
+pwd
+echo
+echo "Your current directory contents are:"
+ls
+echo
+echo "You are logged into the user:"
+whoami
